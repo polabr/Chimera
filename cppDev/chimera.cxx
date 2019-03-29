4f774747c60b67178b1b785f7d5426f19569e42f
Polina Abratenko <polabr@umich.edu>
2019-03-29 16:06:42 -0400

    All 6 vars

--- cppDev/chimera.cxx
+++ cppDev/chimera.cxx
@@ -1,4 +1,4 @@
-// Implementing LLHD for multiple vars (2 for now)
+// Implementation of LLHD for multiple vars 
 
 #include <iostream>
 #include <iomanip>
@@ -17,19 +17,26 @@ Double_t logLLHD_p1(Int_t n) {
   return p1;
 }
 
-Double_t logLLHD_p2(Double_t sigmaSqX, Double_t sigmaSqY) {
+Double_t logLLHD_p2(Double_t sigmaSqX, Double_t sigmaSqY, Double_t sigmaSqZ, Double_t sigmaSqTheta, Double_t sigmaSqPhi, Double_t sigmaSqLen) {
 
-  Double_t p2 = -0.5 * ( TMath::Log( sigmaSqX ) + TMath::Log( sigmaSqY ) ) ;
+  Double_t p2 = -0.5 * ( TMath::Log( sigmaSqX ) + TMath::Log( sigmaSqY ) + TMath::Log( sigmaSqZ ) + TMath::Log( sigmaSqTheta ) + TMath::Log( sigmaSqPhi ) + TMath::Log( sigmaSqLen )) ;
 
   return p2; 
 }
 
-Double_t logLLHD_p3(Double_t sigmaSqX, Double_t x, Double_t muX, Double_t sigmaSqY, Double_t y, Double_t muY ) {
+Double_t logLLHD_p3(Double_t sigmaSqX, Double_t x, Double_t muX, Double_t sigmaSqY, Double_t y, Double_t muY, 
+		    Double_t sigmaSqZ, Double_t z, Double_t muZ, Double_t sigmaSqTheta, Double_t theta, Double_t muTheta, 
+		    Double_t sigmaSqPhi, Double_t phi, Double_t muPhi, Double_t sigmaSqLen, Double_t len, Double_t muLen) {
 
-  if (sigmaSqX == 0.0)
+  if (sigmaSqX == 0.0 || sigmaSqY == 0.0 || sigmaSqZ == 0.0 || sigmaSqTheta == 0.0 || sigmaSqPhi == 0.0 || sigmaSqLen == 0.0) 
     cout << "  Error: The code tries to divide by zero! " << endl;
 
-  Double_t p3 = -0.5 * ( ( x - muX )*( x - muX ) / sigmaSqX + ( y - muY )*( y - muY ) / sigmaSqY );
+  Double_t p3 = -0.5 * ( ( x - muX )*( x - muX ) / sigmaSqX 
+			 + ( y - muY )*( y - muY ) / sigmaSqY 
+			 + ( z - muZ )*( z - muZ ) / sigmaSqZ
+			 + ( theta - muTheta )*( theta - muTheta ) / sigmaSqTheta 
+			 + ( phi - muPhi )*( phi - muPhi ) / sigmaSqPhi 
+			 + ( len - muLen )*( len - muLen ) / sigmaSqLen );
 
   return p3;
 }
@@ -37,16 +44,29 @@ Double_t logLLHD_p3(Double_t sigmaSqX, Double_t x, Double_t muX, Double_t sigmaS
 int main() {
 
   // How many parameters are we looking at?
-  Int_t numParams = 2;
+  Int_t numParams = 6;
 
   // Input "requested" variables
-  Double_t inputX = 202.0;
-  Double_t inputY = 100.7;
-  Double_t desiredSigmaX = 0.001;
+  Double_t inputX = 24.6;
+  Double_t inputY = -47.8;
+  Double_t inputZ = 816.9;
+  Double_t inputTheta = 3.8;
+  Double_t inputPhi = -62.8;
+  Double_t inputLen = 4.8;
+
+  Double_t desiredSigmaX = 100.0;
   Double_t desiredSigmaY = 100.0;
+  Double_t desiredSigmaZ = 100.0;
+  Double_t desiredSigmaTheta = 100.0;
+  Double_t desiredSigmaPhi = 100.0;
+  Double_t desiredSigmaLen = 100.0;
 
   Double_t desiredSigmaSqX = desiredSigmaX * desiredSigmaX;
   Double_t desiredSigmaSqY = desiredSigmaY * desiredSigmaY;
+  Double_t desiredSigmaSqZ = desiredSigmaZ * desiredSigmaZ;
+  Double_t desiredSigmaSqTheta = desiredSigmaTheta * desiredSigmaTheta;
+  Double_t desiredSigmaSqPhi = desiredSigmaPhi * desiredSigmaPhi;
+  Double_t desiredSigmaSqLen = desiredSigmaLen * desiredSigmaLen;
 
   TFile *f1 = new TFile("../data/FinalVertexVariables.root","READ");
 
@@ -59,6 +79,11 @@ int main() {
   int _event;
   float _x;
   float _y;
+  float _z;
+  float Muon_ThetaReco; 
+  float Muon_PhiReco; 
+  float Muon_TrackLength; 
+
   int _passCuts;
   float _cosmiLL;
   
@@ -67,6 +92,10 @@ int main() {
   myTree->SetBranchAddress("event", &_event);
   myTree->SetBranchAddress("Xreco", &_x);
   myTree->SetBranchAddress("Yreco", &_y);
+  myTree->SetBranchAddress("Zreco", &_z);
+  myTree->SetBranchAddress("Muon_ThetaReco", &Muon_ThetaReco);
+  myTree->SetBranchAddress("Muon_PhiReco", &Muon_PhiReco);
+  myTree->SetBranchAddress("Muon_TrackLength", &Muon_TrackLength);
   myTree->SetBranchAddress("PassCuts", &_passCuts);
   myTree->SetBranchAddress("CosmicLL", &_cosmiLL);
 
@@ -83,12 +112,13 @@ int main() {
   int passCutCount = 0;
 
   float x;
-  float y;
+  // rest of vars not needed bc not filling a new ttree for now
 
   TFile *outputTree = new TFile("outputTree.root","RECREATE");
   TTree *_tree = new TTree("tree","Just a Tree");
   _tree->Branch("x", &x, "x/F");
   _tree->Branch("y", &x, "y/F");
+  // rest of vars not needed bc not filling a new ttree for now
   
   Double_t smallestLLHD = 99999999999999999999999999999999999999999999999999999999999.99;
   Double_t closestX = 9999.99;
@@ -114,7 +144,11 @@ int main() {
       cout << "The associated track is: Run: " << _run << " Subrun: " << _subrun << " Event: " << _event << endl; 
 
       // Compute the negative log LLHD here
-      Double_t LLHD_i = - ( logLLHD_p1( numParams ) + logLLHD_p2( desiredSigmaSqX, desiredSigmaSqY ) + logLLHD_p3( desiredSigmaSqX, _x, inputX, desiredSigmaSqY, _y, inputY ) );
+      Double_t LLHD_i = - ( logLLHD_p1( numParams ) 
+			    + logLLHD_p2( desiredSigmaSqX, desiredSigmaSqY, desiredSigmaSqZ, desiredSigmaSqTheta, desiredSigmaSqPhi, desiredSigmaSqLen ) 
+			    + logLLHD_p3( desiredSigmaSqX, _x, inputX, desiredSigmaSqY, _y, inputY, 
+					  desiredSigmaSqZ, _z, inputZ, desiredSigmaSqTheta, Muon_ThetaReco, inputTheta,
+					  desiredSigmaSqPhi, Muon_PhiReco, inputPhi, desiredSigmaSqLen, Muon_TrackLength, inputLen) );
       cout << "LLHD_i = " << LLHD_i << endl;
     
       if (LLHD_i < smallestLLHD) {
