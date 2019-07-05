0def31a43050fd27fd44723bf2b90aff7eda8098
Polina Abratenko <polabr@umich.edu>
2019-07-05 12:39:11 -0500

    Working code to put 2 tracks together

--- cppDev/put2TracksTogether.cxx
+++ cppDev/put2TracksTogether.cxx
@@ -10,13 +10,14 @@
 #include "TFile.h"
 #include "TTree.h"
 #include "/Users/nduser/dllee_unified/LArCV/core/DataFormat/IOManager.h"
+#include "/Users/nduser/dllee_unified/LArCV/core/DataFormat/EventImage2D.h"
 
 using namespace std;
 
 int main() {
 
   // This is the likelihood output tree containing matching pairs of muons & protons
-  TFile *f1 = new TFile("output_likelihood_aab.root","READ");
+  TFile *f1 = new TFile("output_likelihood_smoltest.root","READ");
   TTree *llhdOutputTree = (TTree *)f1->Get("tree");
 
   // Create variables that are in the LLHD output TTree again
@@ -26,6 +27,7 @@ int main() {
   int _chosenMuVtxid;
   int _chosenMuMuID;
   int _chosenMuProtID;
+
   int _chosenPRun;
   int _chosenPSubrun;
   int _chosenPEvent;
@@ -39,6 +41,7 @@ int main() {
   llhdOutputTree->SetBranchAddress("chosenMuVtxid", &_chosenMuVtxid);
   llhdOutputTree->SetBranchAddress("chosenMuMuID", &_chosenMuMuID);
   llhdOutputTree->SetBranchAddress("chosenMuProtID", &_chosenMuProtID);
+
   llhdOutputTree->SetBranchAddress("chosenPRun", &_chosenPRun);
   llhdOutputTree->SetBranchAddress("chosenPSubrun", &_chosenPSubrun);
   llhdOutputTree->SetBranchAddress("chosenPEvent", &_chosenPEvent);
@@ -46,13 +49,16 @@ int main() {
   llhdOutputTree->SetBranchAddress("chosenPMuID", &_chosenPMuID);
   llhdOutputTree->SetBranchAddress("chosenPProtID", &_chosenPProtID);
 
-  // Use IOManager to deal with image2d file
+  // Use IOManager to read input image2d tree
   larcv::IOManager* ioman = new larcv::IOManager(larcv::IOManager::kREAD);
-
-  ioman->add_in_file("myCoolTest.root");
+  ioman->add_in_file("hadded_output.root");
   ioman->initialize();
 
-  // For each track chosen by the likelihood, find the corresponding image2d
+  larcv::IOManager* out_iom = new larcv::IOManager(larcv::IOManager::kWRITE);
+  out_iom->set_out_file("happy4thofjuly.root");
+  out_iom->initialize();
+
+  // For each track chosen by the likelihood, find the corresponding image2ds
   Int_t llhdEntries = llhdOutputTree->GetEntries();
   ULong_t image2dEntries = ioman->get_n_entries();
 
@@ -63,29 +69,131 @@ int main() {
   for (Int_t i = 0; i < llhdEntries; i++) {
 
     llhdOutputTree->GetEntry(i);
+
+    std::vector<larcv::Image2D> out_img_muon(3);
+    std::vector<larcv::Image2D> out_img_proton(3);
+    std::vector<larcv::Image2D> out_img_combined(3);
+    std::vector<larcv::ImageMeta> meta(3);
+
+    bool foundMuon = false;
+    bool foundProton = false;
+
+    Int_t _muonRun; 
+    Int_t _muonSubrun;
+    ULong_t _muonEvent;
+    Int_t _protonRun; 
+    Int_t _protonSubrun;
+    ULong_t _protonEvent;
+    Int_t _combinedRun; 
+    Int_t _combinedSubrun;
+    unsigned long long _combinedEvent;
+    //    uint64_t _combinedEvent;
     
     for (Int_t j = 0; j < image2dEntries; j++) {
 
-      _run = -999;
-      
-      std::cout << _run << std::endl;
-
       ioman->read_entry(j);
+      //      out_iom->read_entry(j);
+      
+      // Include this line every time using IOManager, after getting the entry
+      const auto ev_img = (larcv::EventImage2D*)ioman->get_data( larcv::kProductImage2D, "test" );
+      larcv::EventImage2D* ev_img_out = (larcv::EventImage2D*)out_iom->get_data( larcv::kProductImage2D, "testCombined" );
+      larcv::EventImage2D* ev_img_out_muon = (larcv::EventImage2D*)out_iom->get_data( larcv::kProductImage2D, "testMuon" );
+      larcv::EventImage2D* ev_img_out_proton = (larcv::EventImage2D*)out_iom->get_data( larcv::kProductImage2D, "testProton" );
 
       _run =  ioman->event_id().run();
+      _subrun =  ioman->event_id().subrun();
+      _event =  ioman->event_id().event();
 
-      std::cout << "Run: " << _run << " ChosenMuRun: " << _chosenMuRun << std::endl;
+      if (_run != _chosenMuRun && _run != _chosenPRun) continue;
+      if (_subrun != _chosenMuSubrun && _subrun != _chosenPSubrun) continue;
+      if (_event != (_chosenMuEvent*100 + _chosenMuVtxid*10 + _chosenMuMuID) && _event != (_chosenPEvent*100 + _chosenPVtxid*10 + _chosenPProtID)) continue;
 
-      if (_run == _chosenMuRun) {
+      std::cout << "Run: " << _run << " ChosenMuRun: " << _chosenMuRun << " ChosenPRun: " << _chosenPRun << std::endl;
+      std::cout << "Subrun: " << _subrun << " ChosenMuSubrun: " << _chosenMuSubrun << " ChosenPSubrun: " << _chosenPSubrun << std::endl;
+      std::cout << "Event: " << _event << " ChosenMuEvent: " << _chosenMuEvent << " ChosenPEvent: " << _chosenPEvent << std::endl;
 
-	std::cout << "Found a matching run!" << _run << "=" << _chosenMuRun << std::endl;	
+      if (_run == _chosenMuRun && _subrun == _chosenMuSubrun && _event == ( _chosenMuEvent*100 + _chosenMuVtxid*10 + _chosenMuMuID) ) {
+
+	std::cout << "FOUND A MUON MATCH!!" << std::endl;
+	foundMuon = true;
+	
+	out_img_muon = ev_img->Image2DArray();
+	
+	std::cout << "Event #: " << _event << std::endl;
+	std::cout << "Chosen Mu Event #: " <<  _chosenMuEvent*100 + _chosenMuVtxid*10 + _chosenMuMuID << std::endl;
+
+	_muonRun = _run;
+	_muonSubrun = _subrun;
+	_muonEvent = _event;
 
       }
+      
+      if (_run == _chosenPRun && _subrun == _chosenPSubrun && _event == ( _chosenPEvent*100 + _chosenPVtxid*10 + _chosenPProtID)) {
 
+	std::cout << "FOUND A PROTON MATCH!!" << std::endl;
+	foundProton = true;
 
+	out_img_proton = ev_img->Image2DArray();
+
+	std::cout << "Event #: " << _event << std::endl;
+	std::cout << "Chosen P Event #: " <<  _chosenPEvent*100 + _chosenPVtxid*10 + _chosenPProtID << std::endl;
+
+	_protonRun = _run;
+	_protonSubrun = _subrun;
+	_protonEvent = _event;
+
+      }      
+     
+      if (foundMuon && foundProton) {
+	
+	_combinedRun = _muonRun * 100000 + _protonRun;
+	_combinedSubrun = _muonSubrun * 10000 + _protonSubrun;
+	_combinedEvent = _muonEvent * 100000000 + _protonEvent;
+	
+	for (size_t iPlane= 0; iPlane < 3; iPlane++ ) {
+	  meta[iPlane] = out_img_muon[iPlane].meta();
+	  out_img_combined[iPlane] = out_img_muon[iPlane];
+	  out_img_combined[iPlane].paint(0.0);
+	  for (size_t icol = 0; icol < out_img_muon[iPlane].meta().cols(); icol++) {
+	    for(size_t irow = 0; irow < out_img_muon[iPlane].meta().rows(); irow++) {
+	      out_img_combined[iPlane].set_pixel(irow,icol,out_img_muon[iPlane].pixel(irow, icol) + out_img_proton[iPlane].pixel(irow, icol));
+	      
+	    }
+	  }
+	  
+	  ev_img_out->Emplace(std::move(out_img_combined[iPlane]));
+	  ev_img_out_muon->Emplace(std::move(out_img_muon[iPlane]));
+	  ev_img_out_proton->Emplace(std::move(out_img_proton[iPlane]));
+	  
+	}
+
+	out_iom->set_id(_combinedRun, _combinedSubrun, _combinedEvent);
+	out_iom->save_entry();
+	break;
+	
+	//	for (size_t iPlane= 0; iPlane < 3; iPlane++ ) {
+
+	  
+
+	//	}
+	
+      }
+    
     }
-
   }
+ 
 
+  ioman->finalize();
+  out_iom->finalize();
+  
   return 0;
+
 }
+
+
+// Same meta: single loop, rows and columns
+// Set the pixel after running over both to create a new image2d with both sets of pixels in it
+
+// GetProjectedImagePixel (in UBWireTool)
+
+// Add run, subrun, event 
---
+++ cppDev/readimg.cxx
@@ -0,0 +1,128 @@
+#include <iostream>
+
+// ROOT
+#include "TFile.h"
+#include "TH2D.h"
+#include "TCanvas.h"
+#include "TStyle.h"
+
+// larlite
+#include "DataFormat/storage_manager.h"
+#include "LArUtil/LArProperties.h"
+
+// larcv
+#include "DataFormat/IOManager.h"
+#include "DataFormat/EventImage2D.h"
+#include "DataFormat/EventChStatus.h"
+
+/**
+ * simple macro to read an Image2D and plot it in TH2D
+ *
+ */
+int main( int nargs, char** argv ) {
+
+  gStyle->SetOptStat(0);
+
+  std::string input_larcv  = argv[1]; // full file path of input larcv file 
+  
+  int run, subrun;
+  ULong_t event;
+  
+  // input larcv
+  larcv::IOManager iolarcv( larcv::IOManager::kREAD );
+  iolarcv.add_in_file( input_larcv );
+  iolarcv.initialize();
+
+  /* code below shows how I would save the output
+  // larcv:
+  larcv::IOManager outlarcv( larcv::IOManager::kWRITE, "", larcv::IOManager::kTickBackward );
+  outlarcv.set_out_file( output_larcv );
+  outlarcv.initialize();
+  //larlite:
+  larlite::storage_manager outlarlite( larlite::storage_manager::kWRITE );
+  outlarlite.set_out_filename( output_larlite );
+  outlarlite.open();
+  */
+  std::string output_hist = "cooloutput_4thofjuly.root";
+  TFile* fout = new TFile( output_hist.c_str(), "recreate" ); // do not rewrite
+  TH2D* hadc_muon[3];
+  TH2D* hadc_proton[3];
+  TH2D* hadc_combined[3];
+
+  int nentries = iolarcv.get_n_entries();
+  //  nentries=1;
+  for (int i=0; i<nentries; i++) {
+    iolarcv.read_entry(i);
+
+    std::cout << "entry " << i << std::endl;
+
+    // in
+    auto ev_img_muon = (larcv::EventImage2D*)iolarcv.get_data( larcv::kProductImage2D, "testMuon" );
+    auto ev_img_proton = (larcv::EventImage2D*)iolarcv.get_data( larcv::kProductImage2D, "testProton" );
+    auto ev_img_combined = (larcv::EventImage2D*)iolarcv.get_data( larcv::kProductImage2D, "testCombined" );
+
+    // out: if I am saving Image2Ds
+    //auto evout_wire            = (larcv::EventImage2D*)outlarcv.get_data( larcv::kProductImage2D, "testout");
+    
+    run    = iolarcv.event_id().run();
+    subrun = iolarcv.event_id().subrun();
+    event  = iolarcv.event_id().event();
+
+    std::cout << ev_img_muon->Image2DArray().size() << " " << ev_img_combined->Image2DArray().size() << std::endl;
+
+    //std::cout <<"run "<< run <<" subrun " << subrun <<" event " << event << std::endl;
+
+    for ( int planeid=0; planeid<3; planeid++ ) {
+      auto const& img_muon = ev_img_muon->Image2DArray().at( planeid );
+      auto const& img_proton = ev_img_proton->Image2DArray().at( planeid );
+      auto const& img_combined = ev_img_combined->Image2DArray().at( planeid );
+      auto const& meta = img_muon.meta();
+      char histname_event[100];
+      sprintf(histname_event,"hadc_muon_run%d_event%lu_plane%d",run,event,planeid);
+      hadc_muon[planeid] = new TH2D(histname_event,"",meta.cols(), 0, meta.cols(), meta.rows(),0,meta.rows());
+      sprintf(histname_event,"hadc_proton_run%d_event%lu_plane%d",run,event,planeid);
+      hadc_proton[planeid] = new TH2D(histname_event,"",meta.cols(), 0, meta.cols(), meta.rows(),0,meta.rows());
+      sprintf(histname_event,"hadc_combined_run%d_event%lu_plane%d",run,event,planeid);
+      hadc_combined[planeid] = new TH2D(histname_event,"",meta.cols(), 0, meta.cols(), meta.rows(),0,meta.rows());
+
+      for(int row = 0; row<meta.rows(); row++){
+        for(int col=0; col<meta.cols(); col++){
+          float pixamp = img_muon.pixel( row, col );
+          hadc_muon[planeid]->SetBinContent(col+1, row+1, pixamp);
+          pixamp = img_proton.pixel( row, col );
+          hadc_proton[planeid]->SetBinContent(col+1, row+1, pixamp);
+	  pixamp = img_combined.pixel(row, col);
+          hadc_combined[planeid]->SetBinContent(col+1, row+1, pixamp);
+        }
+      }
+    }
+    // if I am savinf Image2D output: transfer image and chstatus data to output file
+    //for ( auto const& img : ev_img->Image2DArray() ) {
+    //  evout_wire->Append( img );
+    //}
+
+    //for larlite output
+    //outlarlite.set_id( run, subrun, event );
+    //outlarlite.next_event();
+
+    // for larcv output
+    //outlarcv.set_id( run, subrun, event );
+    //outlarcv.save_entry();
+
+    fout->cd();
+    for ( int p=0; p<3; p++ ) {    
+      hadc_muon[p]->Write();
+      hadc_proton[p]->Write();
+      hadc_combined[p]->Write();
+    }
+
+  }//end of entry loop
+  
+  iolarcv.finalize();
+  //outlarcv.finalize();
+  //outlarlite.close();
+  fout->Close();
+
+  
+  return 0;
+}
